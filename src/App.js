d416c4dfa342a7e5ff2d4f26ae69c363586e9ae7
Regina Mc Guire <rmcguire@antelecom.net>
2020-05-14 21:09:14 -0700

    Add About Me component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
+import AboutMe from './components/AboutMe/AboutMe'
 import './main.scss';
-
 function App() {
   return (
     <div className="App">
       <main>
-        <section className="intro">
-        <h1><span>Hi, I'm Regina</span></h1>
-        <h3><span>Let's create beauty together.</span></h3>
-        </section>
+        <AboutMe />
       </main>
     </div>
   );
---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutMe = () => {
+    return (
+        <section className="intro">
+        <h1><span>Hi, I'm Regina</span></h1>
+        <h3><span>Let's create beauty together.</span></h3>
+        </section>
+    )
+}
+export {AboutMe as default}
