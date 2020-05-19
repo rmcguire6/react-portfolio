3601fd6a624cf1fd7d4ddc44a26ea7a50f6ccc5d
Regina McGuire <rmcguire@antelecom.net>
2020-05-18 17:27:32 -0700

    Update styling to match Projects

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AboutMe = () => {
     return (
-        <section className="intro">
+        <section className="section">
         <h1><span className="text text-sm">Hi, I'm Regina</span></h1>
         <h3><span className="text text-sm">Let's create beauty together.</span></h3>
         </section>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,13 +3,13 @@ import React from 'react'
 const NavBar = () => {
     return (
         <nav className='nav'>
-            <a href="#" className="nav__link">
+            <a href="#projects" className="nav__link">
                     <span className="text text--small text--link">
                         Projects
                     </span>
             </a>
             
-            <a href="#" className="nav__link">
+            <a href="#contact" className="nav__link">
                     <span className="text text--small text--link">
                         Contact
                     </span>
