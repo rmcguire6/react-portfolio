1aab5d25002023d52ca08c30d9381cdd1858bbf0
Regina McGuire <rmcguire@antelecom.net>
2020-05-20 16:37:36 -0700

    Refactor styles folder

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import AboutMe from './components/AboutMe/AboutMe'
 import NavBar from './components/NavBar/NavBar'
 import Projects from './components/Projects/Projects'
-import './main.scss';
+import '../src/styles/main.scss';
 function App() {
   return (
     <div className="App">
