18362811692e1639caecc824c26e004d36485e33
Regina McGuire <rmcguire@antelecom.net>
2020-05-19 17:10:12 -0700

    Change styling of NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,14 +3,14 @@ import React from 'react'
 const NavBar = () => {
     return (
         <nav className='nav'>
-            <a href="#projects" className="nav__link">
-                    <span className="text text--small text--link">
+            <a href="#projects" className="btn">
+                    <span className="text">
                         Projects
                     </span>
             </a>
             
-            <a href="#contact" className="nav__link">
-                    <span className="text text--small text--link">
+            <a href="#contact" className="btn">
+                    <span className="text text--sm">
                         Contact
                     </span>
             </a>
