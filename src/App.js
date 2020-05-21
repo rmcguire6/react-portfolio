ca9be1b481ea0f30ef1435d6ac62054f19531c38
Regina McGuire <rmcguire@antelecom.net>
2020-05-21 12:35:15 -0700

    Add Contact component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import AboutMe from './components/AboutMe/AboutMe'
+import Contact from './components/Contact/Contact'
 import NavBar from './components/NavBar/NavBar'
 import Projects from './components/Projects/Projects'
 import '../src/styles/main.scss';
@@ -10,6 +11,7 @@ function App() {
         <NavBar />
         <AboutMe />
         <Projects />
+        <Contact />
     </div>
   );
 }
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <section id="contact">
+        <div className="section__header">
+           <h2 className="text">CONTACT</h2>
+        </div>
+        </section>
+        )
+
+}
+export {Contact as default}
