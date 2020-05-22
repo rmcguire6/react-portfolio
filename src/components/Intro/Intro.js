3df5b0d46e2e857702406f9f15509e93d9c6d30a
Regina McGuire <rmcguire@antelecom.net>
2020-05-22 16:58:52 -0700

    Add header tag

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -3,9 +3,13 @@ import React from 'react'
 const Intro = () => {
     return (
         <section className="section">
-        <h1><span className="text text-sm">Hi, I'm Regina</span></h1>
-        <h3><span className="text text-sm">Let's create beauty together.</span></h3>
-        </section>
-    )
+        <div id="intro">
+            <header>
+                <h1><span className="text text-sm">Hi, I'm Regina</span></h1>
+                <h3><span className="text text-sm">Let's create beauty together.</span></h3>
+            </header>
+        </div>
+    </section>
+)
 }
 export {Intro as default}
