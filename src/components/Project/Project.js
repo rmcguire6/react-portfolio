47520f77e65c8d56477b7923fc2f4b56e094dfb6
Regina McGuire <rmcguire@antelecom.net>
2020-05-22 19:38:40 -0700

    Add Project boilerplate

---
+++ src/components/Project/Project.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Project = () => {
+    return (
+        <section id="Project" className="section">
+        <div className="section__header">
+           <h2 className="text">Project</h2>
+        </div>
+        </section>
+        )
+
+}
+export {Project as default}
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import Project from '../Project/Project'
 
 const Projects = () => {
     return (
         <section id="projects" className="section">
             <div className="section__header">
             <h2 className="text">PROJECTS</h2>
+            </div >
+            <div className="projects __gallery">
+                <Project />
             </div>
         </section>
         )
