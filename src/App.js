a89d16b7218aa854510bddf48f9ba9725cfbf069
Regina McGuire <rmcguire@antelecom.net>
2021-03-20 16:14:21 -0700

    Add content to Contact

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
   return (
     <div className="App">
       <Intro />
-      <AboutMe />
       <Projects />
+      <AboutMe />
       <Contact />
     </div>
   );
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,32 @@
 import React from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab, faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+library.add(fab, faGithub, faLinkedin);
 
 const Contact = () => {
   return (
-    <section id="contact" className="section">
-      <div className="section__header">
-        <h2 className="text">CONTACT</h2>
-      </div>
+    <section className="contact">
+      <h2 className="section__title">CONTACT ME</h2>
+      <p>name: Regina Mc Guire</p>
+      <a href="mailto: reginamcguire16@gmail.com" class="contact__link">
+        reginamcguire16@gmail.com
+      </a>
+      <ul class="social-list">
+        <li class="social-list__item">
+          <a href="https://github.com/rmcguire6" class="social-list__link">
+            <FontAwesomeIcon icon={["fab", "github"]} />
+          </a>
+        </li>
+        <li class="social-list__item">
+          <a
+            href="https://www.linkedin.com/in/regina-mcguire-14352a180/"
+            class="social-list__link"
+          >
+            <FontAwesomeIcon icon={["fab", "linkedin"]} />
+          </a>
+        </li>
+      </ul>
     </section>
   );
 };
