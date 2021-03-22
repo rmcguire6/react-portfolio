d68740481d0112343beb7e9027fb2f4b7cc3a70c
Regina McGuire <rmcguire@antelecom.net>
2021-03-22 11:40:34 -0700

    Correct errors in Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,21 +7,20 @@ library.add(fab, faGithub, faLinkedin);
 const Contact = () => {
   return (
     <section className="contact">
-      <h2 className="section__title">CONTACT ME</h2>
-      <p>name: Regina Mc Guire</p>
-      <a href="mailto: reginamcguire16@gmail.com" class="contact__link">
+      <h2 className="section__title section__title--contact">CONTACT ME</h2>
+      <a href="mailto: reginamcguire16@gmail.com" className="contact__link">
         reginamcguire16@gmail.com
       </a>
-      <ul class="social-list">
-        <li class="social-list__item">
-          <a href="https://github.com/rmcguire6" class="social-list__link">
+      <ul className="social-list">
+        <li className="social-list__item">
+          <a href="https://github.com/rmcguire6" className="social-list__link">
             <FontAwesomeIcon icon={["fab", "github"]} />
           </a>
         </li>
-        <li class="social-list__item">
+        <li className="social-list__item">
           <a
             href="https://www.linkedin.com/in/regina-mcguire-14352a180/"
-            class="social-list__link"
+            className="social-list__link"
           >
             <FontAwesomeIcon icon={["fab", "linkedin"]} />
           </a>
