e678df3cc697d61b852e16dfc5f9b3b468c8e82a
Regina McGuire <rmcguire@antelecom.net>
2021-03-29 17:51:53 -0700

    Correct data in projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,19 +1,19 @@
 const projects = [
   {
     title: "Sound and Silence",
-    image: "./public/assets/project-images/sound_and_silence.png",
+    image: "../images/project-images/sound_and_silence.png",
     live: "https://sound-and-silence.herokuapp.com/asl",
     repo: "https://github.com/rmcguire6/sound_and_silence",
   },
   {
     title: "Critical Space Strike",
-    image: "./public/assets/project-images/spaceships.png",
+    image: "../images/project-images/spaceships.png",
     live: "https://my-spaceships.herokuapp.com",
     repo: "https://github.com/rmcguire6/solo--criticalspacestrike",
   },
   {
     title: "Meteorite Application",
-    image: "./public/assets/project-images/meteorite.png",
+    image: "../images/project-images/meteorite.png",
     live: "https://rmcguire-meteorite.herokuapp.com",
     repo: "https://github.com/rmcguire6/meteorite",
   },
