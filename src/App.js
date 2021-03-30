88d3bf485df70b2ef6e595ae2395b8f896fa5a79
Regina McGuire <rmcguire@antelecom.net>
2021-03-30 13:06:44 -0700

    Merge About and Intro sections

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import React from "react";
 import AboutMe from "./components/AboutMe";
 import Contact from "./components/Contact";
 import Experience from "./components/Experience";
-import Intro from "./components/Intro";
 import Projects from "./components/Projects";
 import "../src/styles/main.scss";
 
 function App() {
   return (
     <div className="App">
-      <Intro />
-      <Projects />
       <AboutMe />
+      <Projects />
       <Experience />
       <Contact />
     </div>
--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,33 +1,39 @@
 import React from "react";
-import desert from "../images/SunsetAtZionPark.jpg";
+import programmer from "../images/woman.jpeg";
 
 const AboutMe = () => {
   return (
     <section className="about-me">
-      <h2 className="section__title section__title--about">Who I am</h2>
+      <h1 className="section__title section__title--about">
+        Hi, I am <span className="accent">Regina McGuire</span>
+      </h1>
       <p className="section__subtitle section__subtitle--about">
         Full Stack React Developer
       </p>
-      <div className="about_me__body">
-        <p>
+      <div className="about-me__body">
+        <p className="about-me__text">
           My mission as a developer is to improve people's lives. I'm especially
           interested in underserved communities. I want to share the benefits I
           have received through coding.
         </p>
-        <p>
+        <p className="about-me__text">
           In university I majored in Computer Mathematics and spent years in
           mathematics education. Now I enjoy web development because it allows
           me to create beauty logically, solve problems persistantly, and learn
           new technologies constantly.
         </p>
-        <p>
+        <p className="about-me__text">
           I'm currently contributing as both an open-source developer and a
           facilitator at Chingu. My current technologies are Javascript, HTML5,
           CSS3, React, Python, Flask, Django, Node, Express, and Postgres. My
           current learning technologies are Next.js and Typescript.
         </p>
       </div>
-      <img src={desert} alt="sunset at Zion Park" className="about-me__img" />
+      <img
+        src={programmer}
+        alt="programmer at keyboard"
+        className="about-me__img"
+      />
     </section>
   );
 };
