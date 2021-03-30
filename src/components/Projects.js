21d3e7cbd06b04db8a976b28923b67e985330ede
Regina McGuire <rmcguire@antelecom.net>
2021-03-30 14:47:58 -0700

    Add project title and description html

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,9 +4,9 @@ import sound_and_silence from "../images/project-images/sound_and_silence.png";
 import spaceships from "../images/project-images/spaceships.png";
 import meteorite from "../images/project-images/meteorite.png";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { fab, faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { fab, faGithub } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-library.add(fab, faGithub, faLinkedin);
+library.add(fab, faGithub);
 
 const Projects = () => {
   return (
@@ -19,40 +19,84 @@ const Projects = () => {
         <div className="project">
           <img
             src={sound_and_silence}
-            alt="project: sound_and_silence"
+            alt="sound_and_silence"
             className="project__img"
           />
-          <div className="social-list">
-            <a href={projects[0].live}>Live Site</a>
-            <a className="social-list__item" href={projects[0].repo}>
-              <FontAwesomeIcon icon={["fab", "github"]} />
-            </a>
+          <div className="project__desc">
+            <h3>{projects[0].title}</h3>
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+              at molestie diam. Mauris aliquam non urna dapibus dignissim.
+              Maecenas sit amet quam nec sem ornare commodo vitae sit amet nunc.
+              Fusce maximus.
+            </p>
+            <ul className="icon-list">
+              <li className="icon-list__item">
+                <a className="icon-list__link" href={projects[0].live}>
+                  Live Site
+                </a>
+              </li>
+              <li className="icon-list__item">
+                <a className="icon-list__link" href={projects[0].repo}>
+                  <FontAwesomeIcon icon={["fab", "github"]} />
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
         <div className="project">
-          <img
-            src={spaceships}
-            alt="project: sound_and_silence"
-            className="project__img"
-          />
-          <div className="social-list">
-            <a href={projects[1].live}>Live Site</a>
-            <a className="social-list__item" href={projects[1].repo}>
-              <FontAwesomeIcon icon={["fab", "github"]} />
-            </a>
+          <img src={spaceships} alt="spaceships" className="project__img" />
+          <div className="project__desc">
+            <h3>{projects[1].title}</h3>
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+              at molestie diam. Mauris aliquam non urna dapibus dignissim.
+              Maecenas sit amet quam nec sem ornare commodo vitae sit amet nunc.
+              Fusce maximus.
+            </p>
+            <ul className="icon-list">
+              <li className="icon-list__item">
+                <a className="icon-list__link" href={projects[1].live}>
+                  Live Site
+                </a>
+              </li>
+              <li className="icon-list__item">
+                <a className="icon-list__link" href={projects[1].repo}>
+                  <FontAwesomeIcon icon={["fab", "github"]} />
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
         <div className="project">
           <img
             src={meteorite}
-            alt="project: sound_and_silence"
+            alt="meteorite project"
             className="project__img"
           />
-          <div className="social-list">
-            <a href={projects[2].live}>Live Site</a>
-            <a className="social-list__item" href={projects[2].repo}>
-              <FontAwesomeIcon icon={["fab", "github"]} />
-            </a>
+          <div className="project__desc">
+            <h3>{projects[2].title}</h3>
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+              at molestie diam. Mauris aliquam non urna dapibus dignissim.
+              Maecenas sit amet quam nec sem ornare commodo vitae sit amet nunc.
+              Fusce maximus.
+            </p>
+            <ul className="icon-list">
+              <li className="icon-list__item">
+                <a className="icon-list__link" href={projects[2].live}>
+                  Live Site
+                </a>
+              </li>
+              <li className="icon-list__item">
+                <a className="icon-list__link" href={projects[2].repo}>
+                  <FontAwesomeIcon icon={["fab", "github"]} />
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
